bd3622ac5a7d183bc249dad1c93872ab410e1da5
FranxJunior <franklinjunior021118@gmail.com>
2025-03-22 01:23:32 -0500

    mejorar la decodificación de nombres y mensajes en la pantalla de mensajes

--- src/screen/rosas-message.tsx
+++ src/screen/rosas-message.tsx
@@ -11,9 +11,11 @@ export default function RosasMessage() {
     <div className=" w-screen h-screen">
       <div className="absolute top-10 md:w-[400px] w-full  bg-white-700 md:left-[100px] z-[10000] p-10 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30  ">
         <h1 className="text-4xl font-bold text-white capitalize">
-          ¡Hola, {name}!
+          ¡Hola, {decodeURIComponent(name || '')}!
         </h1>
-        <p className=" text-lg text-white capitalize pb-4">{message}.</p>
+        <p className=" text-lg text-white capitalize pb-4">
+          {decodeURIComponent(message|| '')}.
+        </p>
         <span className="text-5xl block pt-5">❤️</span>
         <h1 className="titulo"></h1>
       </div>
--- src/service/handle-create-message.tsx
+++ src/service/handle-create-message.tsx
@@ -3,9 +3,8 @@ import { Message } from "../message";
 export function handleCreateMessage({ name, message }: Message) {
   const url = window.location.origin;
 
-  const nameQuery = `name=${name}`;
-  const messageQuery = `message=${message}`;
-  const urlQuery = `${url}/?${nameQuery}&${messageQuery}`;
-
+  const nameQuery = `name=${encodeURIComponent(name)}`;
+  const messageQuery = `message=${encodeURIComponent(message)}`;
+  const urlQuery = `${url}/?${nameQuery}&${messageQuery}`.replace(/%/g, "%25");
   return urlQuery;
 }
